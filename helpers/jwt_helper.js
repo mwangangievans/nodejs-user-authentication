7edb2af50d35913459ccc840f868e818b9f16770
MuindeMwangangi <muindemwangangi2@gmail.com>
2021-06-30 15:44:22 +0300

    refresh token

--- helpers/jwt_helper.js
+++ helpers/jwt_helper.js
@@ -27,7 +27,6 @@ module.exports = {
         const authHeader = req.headers['authorization']
         const bearerToken = authHeader.split(' ')
         const token = bearerToken[1]
-        //const token = authHeader
         JWT.verify(token, process.env.ACCESS_TOKEN_SECRET,(err,payload) =>{
             if(err){
                 const message = err.name === 'jsonWebTokenError' ? 'Unauthorized' : err.message
@@ -36,5 +35,24 @@ module.exports = {
             req.payload = payload
             next()
         })
-    }
+    },
+    signRefreshToken:(userId) =>{
+        return new Promise((resolve, reject) =>{
+            const payload ={}
+            const secret = process.env.REFRESH_TOKEN_SECRET
+            const options = {
+                expiresIn: '1y',
+                issuer:'evans.com',
+                audience:userId,
+            }
+            JWT.sign(payload, secret, options,(err,token) =>{
+                if(err) {
+                console.log(err.message)
+                reject(createError.InternalServerError())
+
+                }
+                resolve(token)
+            })
+        })
+    },
 }
